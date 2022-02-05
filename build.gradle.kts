871869cd720e26163f0d5acdbcb64d46eedb9c33
Andi Lau <andi@herrlau.de>
2022-02-05 20:31:09 +0100

    Init with Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    application
+    kotlin("jvm") version "1.6.10"
+}
+
+group = "de.herrlau"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    testImplementation(kotlin("test"))
+}
