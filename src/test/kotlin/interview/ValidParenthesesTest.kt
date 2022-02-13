e72467146294cbec8af60beedef3bf6b0963dcff
Andi Lau <andi@herrlau.de>
2022-02-13 17:41:26 +0100

    Add Valid Parentheses

---
+++ src/test/kotlin/interview/ValidParenthesesTest.kt
@@ -0,0 +1,41 @@
+package interview
+
+import org.junit.jupiter.api.Test
+import java.util.*
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+// 20. Valid Parentheses
+// https://leetcode.com/problems/valid-parentheses/
+
+class ValidParenthesesTest {
+    private fun String.isValid(): Boolean {
+        val stack = Stack<Char>()
+        forEach {
+            when (it) {
+                in "[({" -> stack.push(it)
+                ')' -> if (stack.pop() != '(') return false
+                ']' -> if (stack.pop() != '[') return false
+                '}' -> if (stack.pop() != '{') return false
+                else -> error("Invalid input: $it")
+            }
+        }
+        return true
+    }
+
+    @Test
+    fun simpleTest() {
+        assertTrue { "()".isValid() }
+    }
+
+    @Test
+    fun simpleTestAllPairs() {
+        assertTrue { "()[]{}".isValid() }
+    }
+
+    @Test
+    fun simpleTestNotMatching() {
+        assertFalse { "(]".isValid() }
+    }
+
+}
