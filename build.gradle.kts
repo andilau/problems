d70467e90e6b2c512d6df6db465a3233314e9559
Andi Lau <andi@herrlau.de>
2022-03-18 16:39:14 +0100

    Mod build add JUnit & AssertJ dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,9 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+
+    testApi("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    testImplementation("org.assertj:assertj-core:3.22.0")
     testImplementation(kotlin("test"))
 }
 
